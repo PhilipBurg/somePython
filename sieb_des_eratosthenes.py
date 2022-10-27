02f91eb70b54958c7cdacd3e4175e017c7cba2a6
Philip Burg <philipburgi@web.de>
2022-10-27 14:49:08 +0200

    little function to computer prime numbers

---
+++ sieb_des_eratosthenes.py
@@ -0,0 +1,15 @@
+def my_sieve(maximum):
+    primes = []
+    numbers = list(range(2, maximum))
+    candidate = 2
+    while candidate * candidate < maximum:
+        for k in range(candidate, maximum, candidate):
+            if k in numbers:
+                numbers.remove(k)
+        primes.append(candidate)
+        # reset for new candidate
+        candidate = numbers[0]
+    return primes + numbers
+
+
+print(my_sieve(50))
