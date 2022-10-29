32e40122ce5c739324891d18a539a80493b34a3b
Philip Burg <philipburgi@web.de>
2022-10-29 14:56:22 +0200

    Built a file that might be useful for filtering a given list. It contains two different ideas.

---
+++ useful functions for web_dev/list_filter.py
@@ -0,0 +1,13 @@
+# two ideas for filtering a given list, in this example the results of a test at school
+
+test_results = [66.00, 75.00, 30.00, 81.00, 12.00, 25.00, 94.00, 90.00, 87.00, 0.00,
+                16.00, 78.00, 99.00, 92.00, 76.00, 14.00, 0.00, 49.5, 94.00, 97.00]
+# students only pass with 50/100 points or more, a unlucky one failed with 49.5
+
+# first idea with lambda function:
+passed_students1 = list(filter(lambda x: x >= 50, test_results))
+print(passed_students1)
+
+# second idea, using list comprehension:
+passed_students2 = [i if i >=50 else "not passed" for i in test_results]
+print(passed_students2)
