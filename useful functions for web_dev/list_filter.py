6cbacabc06cdd42dacb06f2c6c5a78da89d5a965
Philip Burg <philipburgi@web.de>
2022-10-29 15:08:32 +0200

    .

--- useful functions for web_dev/list_filter.py
+++ useful functions for web_dev/list_filter.py
@@ -2,7 +2,7 @@
 
 test_results = [66.00, 75.00, 30.00, 81.00, 12.00, 25.00, 94.00, 90.00, 87.00, 0.00,
                 16.00, 78.00, 99.00, 92.00, 76.00, 14.00, 0.00, 49.5, 94.00, 97.00]
-# students only pass with 50/100 points or more, a unlucky one failed with 49.5
+# students only pass with 50/100 points or more, an unlucky one failed with 49.5
 
 # first idea with lambda function:
 passed_students1 = list(filter(lambda x: x >= 50, test_results))
